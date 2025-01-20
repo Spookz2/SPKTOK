0e55a3586ec77ac56907bfb8398022794893d5cd
Spookz2 <126528847+Spookz2@users.noreply.github.com>
2025-01-19 21:40:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyClnOwKx7q3vGLKK8p-aHcDZSMsuGDqkR0",
+  authDomain: "spktok-9a571.firebaseapp.com",
+  projectId: "spktok-9a571",
+  storageBucket: "spktok-9a571.appspot.com",
+  messagingSenderId: "512466246061",
+  appId: "1:512466246061:web:2fa4f1dd6e0db02442a924",
+  measurementId: "G-4PV2WTMZGZ"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = firebase.auth();
 // Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyClnOwKx7q3vGLKK8p-aHcDZSMsuGDqkR0",
